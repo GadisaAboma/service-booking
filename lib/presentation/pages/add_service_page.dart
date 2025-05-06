6bb152502b3aaf217fc7e4958e1d4f6e98a02631
Gadisa Aboma <gadaboma@gmail.com>
2025-05-06 08:54:48 +0300

    first commit

--- lib/presentation/pages/add_service_page.dart
+++ lib/presentation/pages/add_service_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:service_booking/presentation/controllers/service_controller.dart';
+import 'package:service_booking/presentation/pages/widgets/service_form.dart';
 
 class AddServicePage extends StatelessWidget {
   const AddServicePage({super.key});
