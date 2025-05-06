1782f3877f9ba36849d711f8dac574703de3af85
Gadisa Aboma <gadaboma@gmail.com>
2025-05-06 09:14:25 +0300

    service form added

--- lib/domain/usecases/get_services_usecase.dart
+++ lib/domain/usecases/get_services_usecase.dart
@@ -1,14 +1,13 @@
-
-
 import 'package:service_booking/domain/entities/service_entity.dart';
 import 'package:service_booking/domain/repositories/service_repository.dart';
 
-class GetServiceUseCase {
+
+class GetServicesUseCase {
   final ServiceRepository repository;
 
-  GetServiceUseCase(this.repository);
+  GetServicesUseCase(this.repository);
 
-  Future<ServiceEntity> call(String id) async {
-    return await repository.getService(id);
+  Future<List<ServiceEntity>> call() async {
+    return await repository.getServices();
   }
 }
