7a9ed606b52f86d9f4aaaf17085d1ac46ad0e8ab
Gadisa Aboma <gadaboma@gmail.com>
2025-05-06 09:16:33 +0300

    service form added

--- lib/presentation/bindings/service_binding.dart
+++ lib/presentation/bindings/service_binding.dart
@@ -4,6 +4,8 @@ import 'package:service_booking/data/datasources/remote_data_source.dart';
 import 'package:service_booking/data/repositories/service_repository_impl.dart';
 import 'package:service_booking/domain/repositories/service_repository.dart';
 import 'package:service_booking/domain/usecases/create_service_usecase.dart';
+import 'package:service_booking/domain/usecases/get_service_usecase.dart';
+import 'package:service_booking/domain/usecases/get_services_usecase.dart';
 import 'package:service_booking/presentation/controllers/service_controller.dart';
 
 class ServiceBinding extends Bindings {
@@ -12,14 +14,22 @@ class ServiceBinding extends Bindings {
     Get.lazyPut(() => http.Client());
     Get.lazyPut(
       () => RemoteDataSource(
-        baseUrl: 'https://crudcrud.com', // Replace with your API URL
+        baseUrl: 'https://crudcrud.com', // Replace with your key
         client: Get.find(),
       ),
     );
     Get.lazyPut<ServiceRepository>(
       () => ServiceRepositoryImpl(Get.find<RemoteDataSource>()),
     );
+    Get.lazyPut(() => GetServicesUseCase(Get.find<ServiceRepository>()));
+    Get.lazyPut(() => GetServiceUseCase(Get.find<ServiceRepository>()));
     Get.lazyPut(() => CreateServiceUseCase(Get.find<ServiceRepository>()));
-    Get.lazyPut(() => ServiceController(Get.find<CreateServiceUseCase>()));
+    Get.lazyPut(
+      () => ServiceController(
+        Get.find<GetServicesUseCase>(),
+        Get.find<GetServiceUseCase>(),
+        Get.find<CreateServiceUseCase>(),
+      ),
+    );
   }
 }
